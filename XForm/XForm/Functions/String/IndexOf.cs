b679e902f0af558def211b57ef6da86907a8c653
Ryley Taketa <rtaket@microsoft.com>
2018-01-09 09:15:38 -0800

    Add split and indexof functions

---
+++ XForm/XForm/Functions/String/IndexOf.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Model.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XForm.Commands;
+using XForm.Data;
+using XForm.Types;
+
+namespace XForm.Functions.String
+{
+    internal class IndexOfBuilder : IFunctionBuilder
+    {
+        public string Name => "IndexOf";
+        public string Usage => "IndexOf(Column inputColumn, String value, Integer startIndex = 0)";
+
+        public IDataBatchColumn Build(IDataBatchEnumerator source, WorkflowContext context)
+        {
+            IDataBatchColumn column = context.Parser.NextColumn(source, context);
+            string value = context.Parser.NextString();
+            String8 value8 = String8.Convert(value, new byte[String8.GetLength(value)]);
+            int startIndex = 0;
+
+            if (context.Parser.HasAnotherArgument)
+            {
+                startIndex = context.Parser.NextInteger();
+            }
+
+            return SimpleTransformFunction<String8, int>.Build(Name, source, column, (text) =>
+            {
+                return text.IndexOf(value8, startIndex);
+            });
+        }
+    }
+}
---
+++ XForm/XForm/Functions/String/Split.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Model.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XForm.Commands;
+using XForm.Data;
+using XForm.Types;
+
+namespace XForm.Functions.String
+{
+    internal class SplitBuilder : IFunctionBuilder
+    {
+        public string Name => "Split";
+        public string Usage => "Split(Column inputColumn, Character separator, Integer indexToReturn, String defaultValue = String.Empty)";
+
+        public IDataBatchColumn Build(IDataBatchEnumerator source, WorkflowContext context)
+        {
+            int[] positionArray = null;
+
+            IDataBatchColumn column = context.Parser.NextColumn(source, context);
+            string separator = context.Parser.NextString();
+
+            if (separator.Length != 1)
+            {
+                throw new ArgumentOutOfRangeException("The separator argument must be a single character.");
+            }
+
+            Char separatorChar = separator[0];
+            int indexToReturn = context.Parser.NextInteger();
+
+            if (indexToReturn < 0)
+            {
+                throw new ArgumentOutOfRangeException("The indexToReturn argument must be greater than or equal to zero.");
+            }
+
+            String8 defaultValue = String8.Empty;
+
+            if (context.Parser.HasAnotherArgument)
+            {
+                string defaultString = context.Parser.NextString();
+                defaultValue = String8.Convert(defaultString, new byte[String8.GetLength(defaultString)]);
+            }
+
+            return SimpleTransformFunction<String8, String8>.Build(Name, source, column, (text) => Split(text, separatorChar, indexToReturn, defaultValue, ref positionArray));
+        }
+
+        public static String8 Split(String8 text, char separatorChar, int indexToReturn, String8 defaultValue, ref int[] positionArray)
+        {
+            Allocator.AllocateToSize(ref positionArray, text.Length);
+            String8Set parts = text.Split(separatorChar, positionArray);
+
+            if (parts.Count >= indexToReturn + 1)
+            {
+                return parts[indexToReturn];
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
